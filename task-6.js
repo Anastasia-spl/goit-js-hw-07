527b25ed90e6bef5db02d13187003ff52a786084
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2020-12-22 14:37:36 +0700

    Update task-6.js

--- task-6.js
+++ task-6.js
@@ -1,10 +1,16 @@
 const inputRef = document.querySelector('#validation-input'); 
 
 inputRef.addEventListener('blur', checkSymbolQuantity);
+inputRef.addEventListener('input', removeCheckingClasslist)
 
 function checkSymbolQuantity(event) {
     if (event.target.value.length === Number(inputRef.getAttribute('data-length'))) {
         inputRef.classList.remove('invalid');
         inputRef.classList.add('valid');
     } else {inputRef.classList.add('invalid');}
+}
+
+function removeCheckingClasslist() {
+    inputRef.classList.remove('invalid');
+    inputRef.classList.remove('valid');
 }
