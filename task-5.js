7f8751ba3ef8602555bb6e66c4e993472fc37f4f
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2020-12-22 14:31:09 +0700

    corrects task 5 and 6

--- task-5.js
+++ task-5.js
@@ -2,7 +2,10 @@ const inputRef = document.querySelector('#name-input');
 const greetingRef = document.querySelector('#name-output');
 
 inputRef.addEventListener('input', changeNameOutput);
-inputRef.addEventListener('blur', saveInitialGreeting);
 
-function changeNameOutput(event) { greetingRef.textContent = event.target.value; } 
-function saveInitialGreeting(event) { if (event.target.value = '') {greetingRef.textContent = 'незнакомец'}}
+function changeNameOutput(event) {
+    greetingRef.textContent = event.target.value;
+    if(event.target.value === '') {
+        greetingRef.textContent = 'незнакомец';
+    }
+}
--- task-6.js
+++ task-6.js
@@ -4,6 +4,7 @@ inputRef.addEventListener('blur', checkSymbolQuantity);
 
 function checkSymbolQuantity(event) {
     if (event.target.value.length === Number(inputRef.getAttribute('data-length'))) {
+        inputRef.classList.remove('invalid');
         inputRef.classList.add('valid');
     } else {inputRef.classList.add('invalid');}
 }
